8b5c3ef90877f8a80a0183086864653d885ff46d
Rishabh Mehta <144726868+rishabh20122003@users.noreply.github.com>
2024-08-05 22:46:02 +0530

    Add files via upload

---
+++ ArgumentFunction.java
@@ -0,0 +1,14 @@
+class ArgumentFunction
+{
+void division(int divisor,int dividend)
+{
+int Quotient;
+Quotient=divisor/dividend;
+System.out.println("\n Quotient after division:="+Quotient);
+}
+public static void main (String []args)
+{
+Argument Function; AF=newArgumentFunction();
+AF.division(20,5);
+}
+}
